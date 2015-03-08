36cc2027e9400a5ef33ce16ec7901e2ff3729e70
shaunakv1 <shaunakv1@gmail.com>
2015-03-08 10:16:21 -0700

    Lab 1 done.. Hello World Label

---
+++ iOS/iOS_8_Swift_Labs/Lab01/TempConverter/TempConverter/TemperatureConverter.swift
@@ -0,0 +1,9 @@
+//
+//  TemperatureConverter.swift
+//  TempConverter
+//
+//  Created by Shaunak Vairagare on 3/8/15.
+//  Copyright (c) 2015 Classroom. All rights reserved.
+//
+
+import Foundation
---
+++ iOS/iOS_8_Swift_Labs/Lab01/TempConverter/TempConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TempConverter
+//
+//  Created by Shaunak Vairagare on 3/8/15.
+//  Copyright (c) 2015 Classroom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
